dafadf3d5aa3c375fa6012b8b35dc9ea61df1c0a
hidekatsu <3scr@proton.me>
2026-08-12 20:44:56 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,5 +25,8 @@ en:`<h1>Celeritous Execution</h1>
   <button id="codetorch"></button><button id="gandi"></button>`};
 const main=document.querySelector('.main');if(main)main.innerHTML=content[lang];}
 const file=document.getElementById('file');
-if(file){file.addEventListener('click', function() {
+if(file){file.addEventListener('click',function(){
 window.location.href = "/file";});}
+const press=document.getElementById('press');
+if(press){press.addEventListener('click',function(){
+window.location.href = "press";});}
