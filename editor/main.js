5b5807e1c9e42429ecb400f7c53d7a6d329b040e
hidekatsu <3scr@proton.me>
2026-09-23 11:30:42 +0900

    Update main.js

--- editor/main.js
+++ editor/main.js
@@ -1,6 +1,7 @@
-(()=>{const c=document.getElementById('main');c.insertAdjacentHTML('beforeend',`<div id="load" style="background-color:#5BAB77;height:100dvh">
-<img id="load1" src="https://run.celeritous.xct.f5.si/img/big-block.svg"></div>
-<img id="load2" src="https://run.celeritous.xct.f5.si/img/small-block.svg"></div>
-<img id="load3" src="https://run.celeritous.xct.f5.si/img/big-block.svg"></div>
-<img id="load4" src="https://run.celeritous.xct.f5.si/img/small-block.svg"></div><h>データを読み込み中...</h>`);})();
---> 読み込み先に表示させる(ために、わざわざ読み込み遅く...)
+(()=>{const m=document.querySelector('.main');m.insertAdjacentHTML('beforeend', `
+<div id="load" style="background-color:#5BAB77;height:100dvh">
+<img id="load1" src="https://run.celeritous.xct.f5.si/img/big-block.svg">
+<img id="load2" src="https://run.celeritous.xct.f5.si/img/small-block.svg">
+<img id="load3" src="https://run.celeritous.xct.f5.si/img/big-block.svg">
+<img id="load4" src="https://run.celeritous.xct.f5.si/img/small-block.svg">
+<p>データを読み込み中...</p></div>`);})();
