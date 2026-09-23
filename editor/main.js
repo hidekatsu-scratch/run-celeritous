d5179ce3f9db116938b0c1b3d84c9fbf9695a5e8
hidekatsu <3scr@proton.me>
2026-09-23 11:24:56 +0900

    Create main.js

---
+++ editor/main.js
@@ -0,0 +1,6 @@
+(()=>{const c=document.getElementById('main');c.insertAdjacentHTML('beforeend',`<div id="load" style="background-color:#5BAB77;height:100dvh">
+<img id="load1" src="https://run.celeritous.xct.f5.si/img/big-block.svg"></div>
+<img id="load2" src="https://run.celeritous.xct.f5.si/img/small-block.svg"></div>
+<img id="load3" src="https://run.celeritous.xct.f5.si/img/big-block.svg"></div>
+<img id="load4" src="https://run.celeritous.xct.f5.si/img/small-block.svg"></div><h>データを読み込み中...</h>`);})();
+--> 読み込み先に表示させる(ために、わざわざ読み込み遅く...)
