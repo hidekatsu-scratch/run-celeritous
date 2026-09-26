23f8f41c0731c0eea72ca4a0eb72ae2ea9402fac
hidekatsu <3scr@proton.me>
2026-09-26 16:14:31 +0900

    Update main.js

--- editor/main.js
+++ editor/main.js
@@ -1,10 +1,10 @@
 (()=>{const m=document.querySelector('.main');m.insertAdjacentHTML('beforeend', `
-<div id="load" style="background-color:#5BAB77;height:100dvh">
+<div id="load" style="background-color:#5BAB77;height:100dvh"><div id="load-block">
 <img id="load1" src="https://run.celeritous.xct.f5.si/img/big-block.svg">
 <img id="load2" src="https://run.celeritous.xct.f5.si/img/small-block.svg">
 <img id="load3" src="https://run.celeritous.xct.f5.si/img/big-block.svg">
 <img id="load4" src="https://run.celeritous.xct.f5.si/img/small-block.svg">
-<p>データを読み込み中...</p></div>`);})();
+</div><p>データを読み込み中...</p></div>`);})();
 (()=>{const s=document.createElement("style");s.textContent=`
 html, body{margin:0;padding:0;}
 #load{display:flex;flex-direction:column;
