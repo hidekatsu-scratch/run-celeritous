68b8983f16d80b8d1d8c6384743de34d477b4d7d
hidekatsu <3scr@proton.me>
2026-09-26 16:07:22 +0900

    Update main.js

--- editor/main.js
+++ editor/main.js
@@ -5,3 +5,7 @@
 <img id="load3" src="https://run.celeritous.xct.f5.si/img/big-block.svg">
 <img id="load4" src="https://run.celeritous.xct.f5.si/img/small-block.svg">
 <p>データを読み込み中...</p></div>`);})();
+(()=>{const s=document.createElement("style");s.textContent=`
+html, body{margin:0;padding:0;}
+
+`;document.head.append(s)})()
